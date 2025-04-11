44745a469a3b05066aaec224f695633e1ad9c44a
Ummadi Nithin <ummadinithin999@gmail.com>
2025-04-11 15:45:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,12 @@ require('dotenv').config();
 const app = express();
 connectDB();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
+app.use(cors({
+  origin: 'job-tracker-frontend-2nrsu0afz-nithins-projects-c73b5d21.vercel.app', // your frontend domain
+  credentials: true, // if you're using cookies or auth headers
+}));
 
 app.use("/api/auth", authRoutes); 
 app.use("/api/jobs", jobRoutes);
