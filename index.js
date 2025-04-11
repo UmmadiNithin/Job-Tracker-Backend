6bc872b852016f4dcd205a807a7e571152a61bda
Ummadi Nithin <ummadinithin999@gmail.com>
2025-04-11 15:56:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ connectDB();
 // app.use(cors());
 app.use(express.json());
 app.use(cors({
-  origin: 'https://job-tracker-backend-2-8sat.onrender.com', // your frontend domain
+  origin: 'job-tracker-frontend-2nrsu0afz-nithins-projects-c73b5d21.vercel.app', // your frontend domain
   credentials: true, // if you're using cookies or auth headers
 }));
 
